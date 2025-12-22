496a0c9cf6348dc8b0aa84ea9149c99af50db4ae
Xing-boop-tech <oengchheangsing@gmail.com>
2025-12-22 21:59:39 +0700

    pop-up menu

--- Confirm Order/java.js
+++
@@ -1,8 +0,0 @@
-// Read item name from URL and show it on confirm page
-const params = new URLSearchParams(window.location.search);
-const item = params.get("item");
-
-const itemNameEl = document.getElementById("itemName");
-if (itemNameEl && item) {
-	itemNameEl.textContent = item;
-}
